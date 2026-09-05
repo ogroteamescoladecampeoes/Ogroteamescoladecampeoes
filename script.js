249f76ce19765d68c571d790cecae8367fd46f9d
Ogroteamescoladecampeoes <equipeogroteam@gmail.com>
2026-09-04 23:55:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -252,7 +252,7 @@ document.getElementById('btn-recuperar').addEventListener('click', async functio
     }
 
     try {
-        var { error } = await supabase.auth.resetPasswordForEmail(email, {
+        var { error } = await supabaseClient.auth.resetPasswordForEmail(email, {
             redirectTo: window.location.origin + window.location.pathname
         });
 
